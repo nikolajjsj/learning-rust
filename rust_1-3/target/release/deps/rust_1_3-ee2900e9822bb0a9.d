daa2aa782f1275a185b5126a50b4a0f4d49acfc0
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-19 17:50:38 +0100

    feature: Finish chapter 1.3

---
+++ rust_1-3/target/release/deps/rust_1_3-ee2900e9822bb0a9.d
@@ -0,0 +1,5 @@
+/Users/nikolaj/code/rust/learning-rust/rust_1-3/target/release/deps/rust_1_3-ee2900e9822bb0a9: src/main.rs
+
+/Users/nikolaj/code/rust/learning-rust/rust_1-3/target/release/deps/rust_1_3-ee2900e9822bb0a9.d: src/main.rs
+
+src/main.rs:
---
+++ rust_1-3/target/release/rust_1-3.d
@@ -0,0 +1,1 @@
+/Users/nikolaj/code/rust/learning-rust/rust_1-3/target/release/rust_1-3: /Users/nikolaj/code/rust/learning-rust/rust_1-3/src/main.rs
