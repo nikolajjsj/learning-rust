233e00ece81a78211a336d5925030bc0ad12a873
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-26 10:44:03 +0100

    feature: Finish chapter 12.3

--- rust_12/src/main.rs
+++ rust_12/src/main.rs
@@ -1,16 +1,20 @@
+use rust_12::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = rust_12::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
