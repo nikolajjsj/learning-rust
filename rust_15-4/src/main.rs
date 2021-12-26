fd4b9fd918001ab2784019a50066e5711c912ed1
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-26 15:56:41 +0100

    feature: Finish chapter 15.4

---
+++ rust_15-4/src/main.rs
@@ -0,0 +1,12 @@
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a));
+}
